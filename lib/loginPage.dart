e101bddd724bef8aa012364e7883cb76fc3bbe0d
Dahabu Saidi <dahabusaidi@gmail.com>
2021-07-18 12:57:07 +0300

    Add navigations on both screens

--- lib/loginPage.dart
+++ lib/loginPage.dart
@@ -6,10 +6,58 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+
+  final GlobalKey<FormState> _formKey1 = GlobalKey<FormState>();
+
+  // Defining string variable
+  String email;
+  String name;
+  String password;
+
+  // Defining boolean variables
+  bool _passwordVisible;
+  bool _isLoading;
+
+  // Instantiate state variables
+  void initState(){
+    _passwordVisible = false;
+    _isLoading = false;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container(
-      child: Text("Login Screen"),),
+    return Scaffold(
+        body: Container(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              GestureDetector(
+                onTap: (){
+                  Navigator.pop(context);
+                },
+                behavior: HitTestBehavior.opaque,
+                child: Padding(
+                  padding: EdgeInsets.only(top: 65.0),
+                  child: Center(
+                    child: Container(
+                      height: 48,
+                      width: 48,
+                      decoration: BoxDecoration(
+                        color: Colors.blue[700],
+                        shape: BoxShape.circle,
+                      ),
+                      child: Icon(
+                        Icons.arrow_back,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      )
     );
   }
 }
--- lib/signUpPage.dart
+++ lib/signUpPage.dart
@@ -8,10 +8,58 @@ class signUpPage extends StatefulWidget {
 
 // ignore: camel_case_types
 class _signUpPageState extends State<signUpPage> {
+  final GlobalKey<FormState> _formKey1b = GlobalKey<FormState>();
+
+  // Defining string variable
+  String email;
+  String name;
+  String password;
+
+  // Defining boolean variables
+  bool _passwordVisible;
+  bool _isLoading;
+
+  // Instantiate state variables
+  void initState(){
+    _passwordVisible = false;
+    _isLoading = false;
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container(
-      child: Text("SignUp Screen"),),
+    return Scaffold(
+      body: Container(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              GestureDetector(
+                onTap: (){
+                  Navigator.pop(context);
+                },
+                behavior: HitTestBehavior.opaque,
+                child: Padding(
+                  padding: EdgeInsets.only(top: 65.0),
+                  child: Center(
+                    child: Container(
+                      height: 48,
+                      width: 48,
+                      decoration: BoxDecoration(
+                        color: Colors.blue[700],
+                        shape: BoxShape.circle,
+                      ),
+                      child: Icon(
+                        Icons.arrow_back,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      )
     );
   }
 }
